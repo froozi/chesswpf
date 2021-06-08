a93b513e1e8e897d2edfa047ad70b08b3756ef10
эльдар хайрутдинов <eldar.khairutdinov@yandex.ru>
2021-06-08 11:02:10 +0300

    addLibraryClass

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        class Chess
+        {
+            public int x;
+            public int y;
+
+            public Chess(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public virtual bool Move(int newX, int newY)
+            {
+                return false;
+            }
+        }
+
+        class King : Chess
+        {
+            public King(int x, int y) : base(x, y)
+            {
+
+            }
+
+            public override bool Move(int newX, int newY)
+            {
+                if (Math.Abs(newX - x) <= 1 && Math.Abs(newY - y) <= 1)
+                {
+                    x = newX;
+                    y = newY;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        class Queen : Chess
+        {
+            public Queen(int x, int y) : base(x, y)
+            {
+
+            }
+
+            public override bool Move(int newX, int newY)
+            {
+                if (x == newX || y == newY || Math.Abs(newX - x) == Math.Abs(y - newY))
+                {
+                    x = newX;
+                    y = newY;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        class Rook : Chess
+        {
+            public Rook(int x, int y) : base(x, y)
+            {
+
+            }
+
+            public override bool Move(int newX, int newY)
+            {
+
+                if (x == newX || y == newY)
+                {
+                    x = newX;
+                    y = newY;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        class Knight : Chess
+        {
+            public Knight(int x, int y) : base(x, y)
+            {
+
+            }
+
+            public override bool Move(int newX, int newY)
+            {
+                if (Math.Abs(newX - x) == 1 && Math.Abs(newY - y) == 2 || Math.Abs(newX - x) == 2 && Math.Abs(newY - y) == 1)
+                {
+                    x = newX;
+                    y = newY;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        class Bishop : Chess
+        {
+            public Bishop(int x, int y) : base(x, y)
+            {
+
+            }
+
+            public override bool Move(int newX, int newY)
+            {
+                if (Math.Abs(newX - x) == Math.Abs(newY - y))
+                {
+                    x = newX;
+                    y = newY;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
