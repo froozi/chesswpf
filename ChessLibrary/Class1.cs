5b7064942bf128a424dcdfd4e9959d875b23c060
эльдар хайрутдинов <eldar.khairutdinov@yandex.ru>
2021-06-08 11:18:22 +0300

    addChessFabrica

---
+++ ChessLibrary/Class1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLibrary
+
+{
+    public class Chess
+    {
+        public int x;
+        public int y;
+
+        public Chess(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public virtual bool Move(int newX, int newY)
+        {
+            return false;
+        }
+    }
+
+    public class King : Chess
+    {
+        public King(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(newX - x) <= 1 && Math.Abs(newY - y) <= 1)
+            {
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Queen : Chess
+    {
+        public Queen(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (x == newX || y == newY || Math.Abs(newX - x) == Math.Abs(y - newY))
+            {
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Rook : Chess
+    {
+        public Rook(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+
+            if (x == newX || y == newY)
+            {
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+
+    public class Knight : Chess
+    {
+        public Knight(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(newX - x) == 1 && Math.Abs(newY - y) == 2 || Math.Abs(newX - x) == 2 && Math.Abs(newY - y) == 1)
+            {
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Bishop : Chess
+    {
+        public Bishop(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(newX - x) == Math.Abs(newY - y))
+            {
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ chesswpf/FigureFabrica.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessLibrary;
+
+namespace chesswpf
+{
+    class FigureFabrica
+    {
+        static public Chess Make(string _name, int x, int y)
+        {
+            Chess figure = null;
+            switch (_name)
+            {
+                case "K":
+                    figure = new King(x, y);
+                   
+                    break;
+                case "Q":
+                    figure = new Queen(x, y);
+                    
+                    break;
+                case "B":
+                    figure = new Bishop(x, y);
+                    
+                    break;
+                case "N":
+                    figure = new Knight(x, y);
+                   
+                    break;
+                case "R":
+                    figure = new Rook(x, y);
+                  
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown piece code. Try again.");
+                    break;
+            }
+
+            return figure;
+        }
+
+        static public Chess Make(int pieceCode, int x, int y)
+        {
+            return Make(pieceCode.ToString(), x, y);
+        }
+    }
+}
